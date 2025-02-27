f9dc8f166e6d4aa1cd46def0f2cc0c5ca83bcb69
Nicolas Fernandes <nicolaschfernandes@gmail.com>
2025-02-26 23:51:36 -0300

    ✨ feat: Mensagens de erro
    As mensagens já estão sendo inseridas no front-end, tornando assim muito mais rápida a operação do que pedir pro back-end, e ao mesmo tempo contribuindo na redundância.

--- front-end/script.js
+++ front-end/script.js
@@ -1,6 +1,6 @@
 let data_hoje;
 
-function atribuirMaxCalendario() {
+function atribuirInputMax() {
     let hoje = new Date();
 
     let dia = hoje.getDate();
@@ -22,47 +22,70 @@ function atribuirMaxCalendario() {
     data_hoje = hoje;
 }
 
+function deletarDivisaoErro(){
+    let divisao_erros = document.getElementById("erro_mensagem") ?? undefined;
+
+    if(divisao_erros != undefined){
+        let main = document.getElementsByTagName("main")[0];
+
+        main.removeChild(divisao_erros);
+    }
+}
+
 function validarCampos(acao, data_inicio, data_final) {
     let mensagem_erro = '';
 
     // Verificações
     if(data_inicio > data_hoje) {
-        mensagem_erro += 'A data inícial não pode ser maior que a data atual' + '\n';
+        mensagem_erro += 'A data inícial não pode ser maior que a data atual \n';
     }
     if(data_final > data_hoje) {
-        mensagem_erro += 'A data final não pode ser maior que a data atual' + '\n';
+        mensagem_erro += 'A data final não pode ser maior que a data atual \n';
     }
     if(data_inicio > data_final) {
-        mensagem_erro += 'A data inicial não pode ser maior que a data final' + '\n';
+        mensagem_erro += 'A data inicial não pode ser maior que a data final \n';
     }
     if(data_inicio == '') {
-        mensagem_erro += 'A data inicial não pode estar vazia' + '\n';
+        mensagem_erro += 'A data inicial não pode estar vazia \n';
     }
     if(data_final == '') {
-        mensagem_erro += 'A data final não pode estar vazia' + '\n';
+        mensagem_erro += 'A data final não pode estar vazia \n';
     }
     if(acao == ''){
-        mensagem_erro += 'A ação não pode estar vazia' + '\n';
+        mensagem_erro += 'A ação não pode estar vazia \n';
     } 
 
     if(mensagem_erro == '') { 
-        return undefined 
+        return undefined;
     } else {    
         return mensagem_erro; 
     }
 }
 
+function criarErro(mensagem_erro){
+    let form = document.getElementsByTagName("form")[0];
+
+    let div_erro = document.createElement("div");
+    div_erro.setAttribute("id", "erro_mensagem");
+
+    let texto_erro = document.createElement("p");
+    texto_erro.textContent = mensagem_erro;
+
+    div_erro.appendChild(texto_erro)
+    form.parentNode.insertBefore(div_erro, form.nextSibling);
+}
+
 async function processoRequisicao(){
+    deletarDivisaoErro();
+
     const acao = document.getElementById("acao").value;
     const data_inicio = document.getElementById("data_inicio").value;
     const data_final = document.getElementById("data_final").value;
 
     const mensagem_erro = validarCampos(acao, data_inicio, data_final);
 
-    if(mensagem_erro != undefined) { 
-        console.log(mensagem_erro);
-        
-        // Enviar para o front o(s) erro(s)
+    if(mensagem_erro != undefined) {         
+        criarErro(mensagem_erro)
         return  
     }
     
